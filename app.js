422d051b499393d4521c0747792dad1f894d9954
JordanReady <jready112@gmail.com>
2022-01-15 17:35:14 -0600

    wait for user to input before starting time

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ $(document).ready(function() {
 //global variables
     var currentQuestion;
     var timeLeft = 10;
+    var interval;
+    var score = 0;
+
+    var updateScore = function (amount) {
+        score += amount;
+        $('#score').text(score);
+    };
+
+//start game
+    var startGame = function () {
+        if(!interval) {
+            if (timeLeft === 0) {
+                updateTimeLeft(10);
+                updateScore(-score);
+            }
+            interval = setInterval (function () {
+                updateTimeLeft(-1);
+                if (timeLeft === 0) {
+                    clearInterval (interval);
+                    interval = undefined;
+                }
+            }, 1000);
+        }
+    };
 
 //random number generator
     var randomNumberGenerator = function (size) {
         return Math.ceil(Math.random() * size);
-    }
+    };
 
 //question generator
     var questionGenerator = function () {
@@ -18,7 +42,7 @@ $(document).ready(function() {
         question.equation = String(num1) + " + " + String(num2);
     
         return question;
-    }
+    };
     
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
@@ -29,31 +53,26 @@ $(document).ready(function() {
             renderNewQuestion();
             $('#user-input').val('');
             updateTimeLeft(+1);
+            updateScore(+1);
         }
-    }
+    };
 
     var renderNewQuestion = function () {
         currentQuestion = questionGenerator();
         $('#equation').text(currentQuestion.equation);
-    }
+    };
 
     $('#user-input').on('keyup', function () {
+        startGame();
         checkAnswer(Number($(this).val()), currentQuestion.answer);
     });
 
 //Timer countdown
-    var interval = setInterval(function () {
-        updateTimeLeft(-1);
-        $('#time-left').text(timeLeft);
-        if (timeLeft === 0) {
-            clearInterval(interval);
-        }
-    }, 1000);
 
     var updateTimeLeft = function (amount) {
         timeLeft += amount;
         $('#time-left').text(timeLeft);
-    }
+    };
 
 
     renderNewQuestion();
